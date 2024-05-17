93084b0fed3cb8ae4db4a8f6d58245a9749ec801
Meat <amin.choubai@gmail.com>
2024-05-17 23:34:47 +0200

    maj

--- Japanese.java
+++ Japanese.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Japanese {
     private static Map<String,String> vocabulary = new TreeMap<>();
@@ -8,6 +11,7 @@ public class Japanese {
 
     static{
         vocabulary.put("あたま", "tête");
+        vocabulary.put("あたまtaguele","taguele");
     }
 
     private void typemachine(String text){
@@ -21,11 +25,19 @@ public class Japanese {
         }
         System.out.println(); // Ajout d'un saut de ligne à la fin du texte
     }
+
+    private List<Integer> training(int times){
+        List<Integer> quiz = new ArrayList<>();
+        for(int i=0;i<times;i++){
+            quiz.add(ThreadLocalRandom.current().nextInt(0, vocabulary.size()+0));
+        }
+        return quiz;
+    }
     
 
     public Japanese(){
         Scanner sc = new Scanner(System.in);
-        typemachine("Salut, es-tu-prêt pour un nouveau entrainement de japonais ?");
+        typemachine("Salut, es-tu-prêt pour un nouveau entrainement de japonais ? :)))");
         typemachine("(oui - non):");
         String input ="";
         while(input.compareToIgnoreCase("oui")!=0 && input.compareToIgnoreCase("non")!=0){
@@ -35,11 +47,40 @@ public class Japanese {
                typemachine("(oui - non):");
             }
             else if (input.compareToIgnoreCase("non")==0) {
-                typemachine("Ça marche, à plus.");
+                typemachine("Ça marche, à plus. :C");
                 return;
             }
         }
-        typemachine("C'est Partiiiii !!! :)");
+        typemachine("C'est Partiiiii !!! :D");
+        typemachine("Combien de mots veux tu réviser ? :)))");
+        int input2 = -1;
+        while(input2<=0){
+            input2=sc.nextInt();
+            if(input2<=0){
+                typemachine("Je n'ai pas compris, Combien de mots veux tu réviser ?");
+            }
+        }
+        typemachine("Parfait, c'est partiiiii !!! :)))");
+        int points = 0;
+        List<Integer> quiz = training(input2);
+        for(int i=0;i<quiz.size();i++){
+            String answer = vocabulary.keySet().toArray()[i].toString();
+            System.out.println(" REPONSE : " + answer);
+
+            
+            typemachine("Que veux dire le mot suivant : ");
+            input = sc.nextLine();
+            if(input.compareToIgnoreCase(vocabulary.get(vocabulary.keySet().toArray()[i]))==0){
+                typemachine("bien :)");
+                points=points+1;
+            }
+            else{
+                typemachine("non :( c'était le mot " + answer);
+            }
+        }
+
+
+
     }
 
     public static void main(String[] args) {
